7dbd36fb9860a0f0ed777a00c04a35961dd20b9d
Mayss Aldawsari <mayss.aldawsari@gmail.com>
2023-11-27 13:58:27 +0000

    Add filter, sorting, and seach mechanism for the task table

--- tasks/views.py
+++ tasks/views.py
@@ -4,13 +4,15 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.exceptions import ImproperlyConfigured
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect, render, get_object_or_404
 from django.views import View
 from django.views.generic.edit import FormView, UpdateView
 from django.urls import reverse
 from tasks.forms import LogInForm, PasswordForm, UserForm, SignUpForm, CreateTaskForm
 from tasks.helpers import login_prohibited
 from .models import Task
+from django.views.decorators.http import require_POST
+from django.utils.dateparse import parse_date
 
 @login_required
 def dashboard(request):
@@ -41,8 +43,68 @@ def create_task(request):
     return render(request, 'create_task.html', {'form': form})
 
 def task_list(request):
-    tasks = Task.objects.order_by('due_date')
-    return render(request, 'task_list.html', {'tasks': tasks})
+    sort_by = request.GET.get('sort_by', 'due_date')  
+
+    if sort_by == 'priority':
+        tasks = Task.objects.order_by('priority')  
+    elif sort_by == 'status':
+        tasks = Task.objects.order_by('-status')  # incomplete tasks first
+    elif sort_by == 'task_title':
+        tasks = Task.objects.order_by('task_title')  
+    else:  
+        tasks = Task.objects.order_by('due_date')
+ 
+    filter_by = request.GET.get('filter_by', '')  
+
+    start_date = request.GET.get('start_date')
+    end_date = request.GET.get('end_date')
+
+    if filter_by == 'date_range' and start_date and end_date:
+        start_date = parse_date(start_date)
+        end_date = parse_date(end_date)
+        tasks = Task.objects.filter(due_date__range=[start_date, end_date])
+    elif filter_by == 'high_priority':
+        tasks = Task.objects.filter(priority='HI')  
+    elif filter_by == 'med_priority':
+        tasks = Task.objects.filter(priority='MD') 
+    elif filter_by == 'low_priority':
+        tasks = Task.objects.filter(priority='LW') 
+    elif filter_by == 'incomp_status':
+        tasks = Task.objects.filter(status='NOT_STARTED') 
+    elif filter_by == 'comp_status':
+        tasks = Task.objects.filter(status='COMPLETED') 
+
+    search = request.GET.get('search', '')  
+
+    if search:
+        tasks = Task.objects.filter(task_title__icontains=search)
+    
+
+    return render(request, 'task_list.html', {'tasks': tasks, 'sort_by': sort_by, 'filter_by': filter_by})
+
+def task_detail(request, task_title):
+    task = get_object_or_404(Task, pk=task_title)
+    return render(request, 'task_detail.html', {'task': task})
+
+
+def edit_task(request, task_title):
+    task = get_object_or_404(Task, task_title=task_title)
+    if request.method == 'POST':
+        form = CreateTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')  
+    else:
+        form = CreateTaskForm(instance=task)
+    return render(request, 'edit_task.html', {'form': form, 'task': task})
+
+
+@require_POST
+def mark_task_complete(request, task_title):
+    task = get_object_or_404(Task, task_title=task_title)
+    task.status = 'COMPLETED'
+    task.save()
+    return redirect('task_list') 
 
 class LoginProhibitedMixin:
     """Mixin that redirects when a user is logged in."""
